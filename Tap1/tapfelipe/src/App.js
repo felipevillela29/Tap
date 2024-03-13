f433075d362e2ba3150427dcbd07273122a48ed7
Felipe V. Souza <felipevillela.2006@gmail.com>
2024-03-13 13:42:07 -0300

    Projeto avaliativo 01

---
+++ Tap1/tapfelipe/src/App.js
@@ -0,0 +1,21 @@
+import logo from './bmw.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          BMW
+       </h1>
+        <img src={logo} className="App-logo" alt="logo" />
+       
+      <h1>Desperte sua paixão pela estrada.</h1>   
+       <h1>BMW: O poder da performance.</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
